f47ae8b213667d7dcfe9817dfc5b36444a5cb98b
rokrupnik <rok7rupnik@gmail.com>
2017-07-30 19:25:57 +0200

    Modified download script. Added checking if asc file is allready downloaded.

--- scripts/download.sh
+++ scripts/download.sh
@@ -6,7 +6,7 @@ do
     for ((y=56;y<=84;y++));
     do
         # If download successful, call asc2png, and if conversion is successfull, delete the asc file
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -15,7 +15,7 @@ for ((x=418;x<=466;x++));
 do
     for ((y=38;y<=84;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -24,7 +24,7 @@ for ((x=445;x<=493;x++));
 do
     for ((y=35;y<=84;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -33,7 +33,7 @@ for ((x=500;x<=556;x++));
 do
     for ((y=69;y<=114;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -42,7 +42,7 @@ for ((x=414;x<=450;x++));
 do
     for ((y=34;y<=65;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -51,7 +51,7 @@ for ((x=474;x<=531;x++));
 do
     for ((y=31;y<=84;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -60,7 +60,7 @@ for ((x=387;x<=418;x++));
 do
     for ((y=32;y<=54;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -69,7 +69,7 @@ for ((x=500;x<=544;x++));
 do
     for ((y=107;y<=139;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -78,7 +78,7 @@ for ((x=486;x<=544;x++));
 do
     for ((y=140;y<=169;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -87,7 +87,7 @@ for ((x=572;x<=623;x++));
 do
     for ((y=127;y<=194;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -96,7 +96,7 @@ for ((x=554;x<=571;x++));
 do
     for ((y=135;y<=176;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -105,7 +105,7 @@ for ((x=539;x<=571;x++));
 do
     for ((y=115;y<=174;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -114,7 +114,7 @@ for ((x=399;x<=499;x++));
 do
     for ((y=127;y<=154;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -123,7 +123,7 @@ for ((x=406;x<=452;x++));
 do
     for ((y=85;y<=122;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -132,7 +132,7 @@ for ((x=375;x<=427;x++));
 do
     for ((y=85;y<=127;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -141,7 +141,7 @@ for ((x=475;x<=499;x++));
 do
     for ((y=111;y<=135;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -150,7 +150,7 @@ for ((x=453;x<=499;x++));
 do
     for ((y=85;y<=110;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -159,7 +159,7 @@ for ((x=425;x<=474;x++));
 do
     for ((y=111;y<=145;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
 
@@ -168,6 +168,6 @@ for ((x=374;x<=438;x++));
 do
     for ((y=115;y<=148;y++));
     do
-        [ ! -e "../tiles/10/"$x"_"$y".png" ] && wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc" && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
+        [ ! -e "../tiles/10/"$x"_"$y".png" ] && { [ -e "TM1_"$x"_"$y".asc" ] || wget "http://gis.arso.gov.si/lidar/dmr1/b_"$b"/D96TM/TM1_"$x"_"$y".asc"; } && python ../../scripts/asc2png.py $x $y && rm "TM1_"$x"_"$y".asc"
     done
 done
